1cb78c90a375c73a14d2cf870bd92a6c469e1f2b
Ricky Liu <ieei.liu@gmail.com>
2022-08-06 13:59:41 +0800

    Add project files.

---
+++ allure-docker-service-ui-blazor/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace allure_docker_service_ui_blazor.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
